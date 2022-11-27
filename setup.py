44f69b3946314a601973207e4b07845e0308a270
sumit199 <bhadouriasumit27@gmail.com>
2022-11-27 15:47:36 +0530

    update

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ setup(
     name="sensor",
     version="0.0.1",
     author="ineuron",
-    author_email="bhadouriasumit27@gmail.com"
-    
+    author_email="bhadouriasumit27@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirement(),
 
 
 )
