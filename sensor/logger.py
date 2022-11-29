248a7dcc6f0324e8f05226621e6dbcd67df651de
sumit199 <bhadouiriasumit27@gmail.com>
2022-11-29 23:31:16 +0530

    update

--- sensor/logger.py
+++ sensor/logger.py
@@ -1,9 +1,9 @@
 import logging
 import os
-from datatime import datatime
+from datetime import datetime
 
 #log file name
-LOG_FILE_NAME = f"{datatime.now().strftime('%m%d%Y__%H%M%S')}.log"
+LOG_FILE_NAME = f"{datetime.now().strftime('%m%d%Y__%H%M%S')}.log"
 
 #log directory
 LOG_FILE_DIR = os.path.join(os.getcwd(),"logs")
@@ -18,5 +18,5 @@ LOG_FILE_PATH = os.path.join(LOG_FILE_DIR,LOG_FILE_NAME)
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO,
+    level=logging.DEBUG,
 )
