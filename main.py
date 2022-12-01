d455565bb52c79fbb3fad00140ab510943fac7fa
sumit199 <bhadouriasumit27@gmail.com>
2022-12-01 21:41:40 +0530

    data ingestion updated

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sensor.exception import SensorException
 from sensor.utils import get_collection_as_dataframe
 import sys,os
 from sensor.entity import config_entity
-from sensor.components.data_ingestion import DataIngestion
+from sensor.component.data_ingestion import DataIngestion
 
 
 
