bb750278274cdcc18d081601d47b528b85eafa1b
sumit199 <bhadouriasumit27@gmail.com>
2022-12-17 22:15:53 +0530

    update data validation

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sensor.utils import get_collection_as_dataframe
 import sys,os
 from sensor.entity import config_entity
 from sensor.component.data_ingestion import DataIngestion
+from sensor.component.data_validation import DataValidation
 
 
 
@@ -13,6 +14,13 @@ if __name__=="__main__":
           data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
           print(data_ingestion_config.to_dict())
           data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
-          print(data_ingestion.initiate_data_ingestion())
+          data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        
+        #data validation
+          data_validation_config = config_entity.DataValidationConfig(training_pipeline_config=training_pipeline_config)
+          data_validation = DataValidation(data_validation_config=data_validation_config,
+                                           data_ingestion_artifact=data_ingestion_artifact)
+
+          data_validation_artifact = data_validation.initiate_data_validation()
      except Exception as e:
           print(e)
--- sensor/component/data_ingestion.py
+++ sensor/component/data_ingestion.py
@@ -12,6 +12,7 @@ class DataIngestion:
     
     def __init__(self,data_ingestion_config:config_entity.DataIngestionConfig ):
         try:
+            logging.info(f"{'>>'*20} Data Ingestion {'<<'*20}")
             self.data_ingestion_config = data_ingestion_config
         except Exception as e:
             raise SensorException(e, sys)
@@ -41,7 +42,7 @@ class DataIngestion:
 
             logging.info("split dataset into train and test set")
             #split dataset into train and test set
-            train_df,test_df = train_test_split(df,test_size=self.data_ingestion_config.test_size)
+            train_df,test_df = train_test_split(df,test_size=self.data_ingestion_config.test_size,random_state=42)
             
             logging.info("create dataset directory folder if not available")
             #create dataset directory folder if not available
